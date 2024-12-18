6617564cceeb99c1ddd63cd006f626bb5fbbce06
khodakarami <khodakarami_dr@outlook.com>
2024-12-18 20:19:11 +0330

    Second Iteration of Structure
    Based on Linguardium's suggestions

---
+++ src/main/java/jiraiyah/uio/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+	public static final String MOD_ID = "uio";
+
+	@Override
+	public void onInitialize() 
+	{
+	}
+}
